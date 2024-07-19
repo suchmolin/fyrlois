f6aa8a4e4cae7cf4f7d5a3639dccc5f75bd3cc15
programadorlerobotica <jsuchmolin.ads@gmail.com>
2024-07-19 10:23:42 -0400

    redireccion a plan vacacional

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,21 @@
+"use client"
 import Contactanos from "@/components/Contactanos/page"
 import Footer from "@/components/Footer/page"
 import MainSection from "@/components/MainSection/page"
 import NavBar from "@/components/NavBar/page"
 import SecondSection from "@/components/SecondSection/page"
+import { useEffect } from "react"
 export default function Home() {
+  useEffect(() => {
+    window.location.replace("https://fyrlois.com.ve/planvacacional")
+  }, [])
   return (
     <main className="w-full font-[lato] pb-[50px]">
-      <NavBar />
+      {/* <NavBar />
       <MainSection />
-      {/*<SecondSection />
-      <Contactanos />*/}
-      <Footer />
+     <SecondSection />
+      <Contactanos />
+      <Footer />*/}
     </main>
   )
 }
