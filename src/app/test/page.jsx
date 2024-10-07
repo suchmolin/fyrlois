120f35c37b970122f9545daa0fadac5a843be9c3
suchmolin <suchmolin11@gmail.com>
2024-10-07 16:47:56 -0400

    test ruta

---
+++ src/app/test/page.jsx
@@ -0,0 +1,7 @@
+"use client"
+export default function TestPage() {
+  window.location.replace(
+    "https://docs.google.com/forms/d/e/1FAIpQLSdiiOaso_9idAARO2Zd8tL8K9qGM3DSyTvZD8bW13GR0VjnZg/viewform"
+  )
+  return true
+}
