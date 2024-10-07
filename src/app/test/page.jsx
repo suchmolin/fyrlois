f2d345457078e20d4a85022ccf7e6256ef9816b6
suchmolin <suchmolin11@gmail.com>
2024-10-07 16:55:16 -0400

    [fix] test relocation

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,7 +1,13 @@
 "use client"
+
+import { useEffect } from "react"
+
 export default function TestPage() {
-  window.location.replace(
-    "https://docs.google.com/forms/d/e/1FAIpQLSdiiOaso_9idAARO2Zd8tL8K9qGM3DSyTvZD8bW13GR0VjnZg/viewform"
-  )
+  useEffect(() => {
+    window.location.replace(
+      "https://docs.google.com/forms/d/e/1FAIpQLSdiiOaso_9idAARO2Zd8tL8K9qGM3DSyTvZD8bW13GR0VjnZg/viewform"
+    )
+  }, [])
+
   return true
 }
