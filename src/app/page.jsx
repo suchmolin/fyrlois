c491a966bdd71a3e8013dd6745a8bee4c44160a9
suchmolin <suchmolin11@gmail.com>
2024-11-11 08:56:53 -0400

    servidor caido

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,15 @@
-"use client";
+"use client"
 
-import Footer from "@/components/Footer/page";
-import MainSection from "@/components/MainSection/page";
-import NavBar from "@/components/NavBar/page";
+import Footer from "@/components/Footer/page"
+import MainSection from "@/components/MainSection/page"
+import NavBar from "@/components/NavBar/page"
 
 export default function Home() {
   return (
     <main className="w-full font-[lato] pb-[50px]">
       <NavBar />
       <MainSection />
-      {/*
-     <SecondSection />
-      <Contactanos />
-      */}
       <Footer />
     </main>
-  );
+  )
 }
